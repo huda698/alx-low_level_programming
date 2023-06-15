1bec97dbce5b302ba0624f4632ffe756aeafae18
huda <hodamohammed076@gmail.com>
2023-06-15 16:08:34 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: 1 (Not Success)
+ */
+
+int main(void)
+{
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, q, 59);
+	return (1);
+}
