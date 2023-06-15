8bd72d45ee23aa81e250d1d04413c373ce992dae
huda <hodamohammed076@gmail.com>
2023-06-15 16:10:18 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- *
  * Return: 1 (Not Success)
  */
 
 int main(void)
 {
 	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(1, q, 59);
 	return (1);
 }
