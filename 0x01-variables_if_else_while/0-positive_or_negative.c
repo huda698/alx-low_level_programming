0826fedea1be96ba95f64ab252e8145971179ea9
huda <hodamohammed076@gmail.com>
2023-06-16 13:04:47 -0700

    printing random numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,8 +19,8 @@ int main(void)
         if (n > 0)
 		printf("%i is positive\n", n);
         else if (n < 0)
-		printf("%i is positive\n", n);
+		printf("%i is negative\n", n);
         else
-		printf("%i is positive\n", n);
+		printf("%i is zero\n", n);
         return (0);
 }
