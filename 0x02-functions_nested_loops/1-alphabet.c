e2fa3bd8126459a58cfbe9c9c9798553b817d788
huda <hodamohammed076@gmail.com>
2023-06-20 16:17:44 -0700

    print lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print lowercase alphabet.
+ */
+
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
