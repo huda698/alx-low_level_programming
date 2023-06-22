3304a1ce4c81911af120f757ab89779ddd31b7f8
huda <hodamohammed076@gmail.com>
2023-06-21 17:27:38 -0700

    print even number

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -4,28 +4,26 @@
 /**
  * main - Entry point
  *
- * this is a program that prints the first 50 Fibonacci numbers.
+ * this is a program that prints the sum of even Fibonacci numbers.
  *
  * Return: 0 (Success).
  */
 
 int main(void)
 {
-	int i;
 	unsigned long fib1 = 0, fib2 = 1, sum;
+	float net;
 
-	for (i = 1; i <= 50; i++)
+	while (1)
 	{
 		sum = fib1 + fib2;
-		if (sum % 2 == 0)
-		{
-			printf("%lu", sum);
-			printf(", ");
-		}
-
+		if (sum > 4000000)
+			break;
+		if ( sum % 2 == 0)
+			net += sum;
 		fib1 = fib2;
 		fib2 = sum;
 	}
-	printf("\n");
+	printf("%.0f\n", net);
 	return (0);
 }
