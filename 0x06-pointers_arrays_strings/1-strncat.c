afb686760a30f67bf9e795c30dccf93b9c3c541d
huda <hodamohammed076@gmail.com>
2023-06-29 14:02:54 -0700

    concatenates two strings.

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,9 +17,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	j = 0;
-	while (dest[j] != '\0')
-	       j++;
+	j = strlen(dest);
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[j + i] = src[i];
