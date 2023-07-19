96a21414b32eab53c8ee4c3e01e070dfcb4e47db
huda <hodamohammed076@gmail.com>
2023-07-19 09:59:47 -0700

    executes a function given as a parameter on each element of an array.

---
+++ 0x0F-function_pointers/putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: print _putchar using putchar prototype.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char s[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(s[i]);
+	_putchar('\n');
+	return (0);
+}
