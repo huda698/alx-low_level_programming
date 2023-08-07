aa22caab55feeaf3dff3c7bcb6d213ca418f4b90
huda <hodamohammed076@gmail.com>
2023-08-07 16:13:04 -0700

    reads a text file and prints it.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,15 +12,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int d;
 	ssize_t b;
-	char buf[READ_BUF_SIZE * 8];
+	char bf[READ_BUF_SIZE * 8];
 
 	if (!filename || !letters)
 		return (0);
-	d = open(filename, O_RONLY);
+	d = open(filename, O_RDONLY);
 	if (d == -1)
 		return (0);
-	b = read(d, &buf(0), letters);
-	b = write(STDOUT_FILENO, &buf(0), b);
+	b = read(d, &bf[0], letters);
+	b = write(STDOUT_FILENO, &bf[0], b);
 	close(d);
 	return (b);
 }
