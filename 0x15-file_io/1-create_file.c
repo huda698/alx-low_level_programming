19387604a48d85ab7ac8f1afcf68f4133f217794
huda <hodamohammed076@gmail.com>
2023-08-07 16:38:41 -0700

    create file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	d = open(filename, O_WRONLY, O_CREAT, O_TRUNC, S_IRUSR, S_IWUSR);
+	d = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (d == -1)
 		return (-1);
 	if (l)
