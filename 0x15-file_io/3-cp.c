0b67c97464ec9e81b8248b3af901445f097c1da5
huda <hodamohammed076@gmail.com>
2023-08-07 17:26:06 -0700

    copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,28 +15,29 @@
 
 int main(int ac, char **av)
 {
-	int from_d = 0, to_d = 0;
+	int from_fd = 0, to_fd = 0;
 	ssize_t b;
 	char bf[READ_BUF_SIZE];
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, USAGE), exit(97);
-	from_d = open(av[1], O_RDONLY);
-	if (from_d == -1)
+	from_fd = open(av[1], O_RDONLY);
+	if (from_fd == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
-	to_d = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
-	if (to_d == -1)
+	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+	if (to_fd == -1)
 		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
-	while ((b = read(from_d, bf, READ_BUF_SIZE)) > 0)
-		if (write(to_d, bf, b) != b)
+
+	while ((b = read(from_fd, bf, READ_BUF_SIZE)) > 0)
+		if (write(to_fd, bf, b) != b)
 			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 	if (b == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
-	from_d = close(from_d);
-	to_d = close(to_d);
-	if (from_d)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_d), exit(100);
-	if (to_d)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_d), exit(100);
+	from_fd = close(from_fd);
+	to_fd = close(to_fd);
+	if (from_fd)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
+	if (to_fd)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
 	return (EXIT_SUCCESS);
 }
