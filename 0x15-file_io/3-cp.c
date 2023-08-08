a74c630ce4a847a0b7667cc8bf872bc1f7981e9a
huda <hodamohammed076@gmail.com>
2023-08-07 17:16:22 -0700

    copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 #define USAGE "Usage: cp file_from file_to\n"
-#define ERR_NOREAD "Error: Can't read from file NAME_OF_THE_FILE%s\n"
-#define ERR_NOWRITE "Error: Can't write to NAME_OF_THE_FILE%s\n"
-#define ERR_NOCLOSE "Error: Can't close fd FD_VALUE%d\n"
+#define ERR_NOREAD "Error: Can't read from file%s\n"
+#define ERR_NOWRITE "Error: Can't write to%s\n"
+#define ERR_NOCLOSE "Error: Can't close fd%d\n"
 #define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 
 /**
