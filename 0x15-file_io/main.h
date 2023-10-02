24e03ea94f05ce9b5b35ff2ed06ffa2673bd4e50
huda <hodamohammed076@gmail.com>
2023-10-02 08:29:28 -0700

    edit main file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <elf.h>
 
 #define READ_BUF_SIZE 1024
 
@@ -12,5 +13,15 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int ac, char **av);
+void print_magic(Elf64_Ehdr h);
+void print_class(Elf64_Ehdr h);
+void print_data(Elf64_Ehdr h);
+void print_version(Elf64_Ehdr h);
+void print_osabi(Elf64_Ehdr h);
+void print_abiversion(Elf64_Ehdr h);
+void print_type(Elf64_Ehdr h);
+void print_entry(Elf64_Ehdr h);
+void print_osabi_more(Elf64_Ehdr h);
 
 #endif
